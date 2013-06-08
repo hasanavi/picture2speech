f90b4f7acb6dbecb157dd0d6f0efbf3f8c204509
Hasan Haque <hasan@movementlondon.com>
2013-06-08 21:40:20 +0100

    face and scene detection separate out

--- api.php
+++ api.php
@@ -4,18 +4,22 @@ include_once 'config.php';
 if(isset($_GET['imgloc']) && strlen($_GET['imgloc']) > 0){
 	$imgloc = $_GET['imgloc'];
 	
-	$faceSceneInfo = faceAndSceneDetection($imgloc);
+	$faceInfo = detectFaceScene($imgloc,'face');
+	//$sceneInfo = detectFaceScene($imgloc,'scene');
 	
 }else{
 	echo 'sory not a valid image location';
 }
 
-function faceAndSceneDetection($imgloc){
+function detectFaceScene($imgloc,$detection){
 	$url= "http://rekognition.com/func/api/?"
 			."api_key=".REKOGNITION_API_KEY
-			."&api_secret=".REKOGNITION_SECRET_SCENE
-			."&jobs=".DETECTION_STRING
-			."&urls=".$imgloc;
+			."&api_secret=".REKOGNITION_SECRET_SCENE;
+	if($detection == 'face')
+		$url .=	"&jobs=".FACE_DETECTION_STRING;
+	else
+		$url .=	"&jobs=".SCENE_UNDERSTANDING_STRING;
+	$url .= "&urls=".$imgloc;
 	
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -27,6 +31,7 @@ function faceAndSceneDetection($imgloc){
 		echo '<pre>';
 		print_r($peopleInfo);
 		echo '</pre>';die;
+		//write your data to human string here
 		
 	}else
 		return 'notvalid';
--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php 
 	define('REKOGNITION_API_KEY', 'kQggHMhxOS2AAEC1');
 	define('REKOGNITION_SECRET_SCENE','9R0Wx0FN297KWQgk');
-	define('DETECTION_STRING','scene_face_gender_emotion_age_glass');
+	define('FACE_DETECTION_STRING','face_gender_emotion_age_glass_aggressive');
+	define('SCENE_UNDERSTANDING_STRING','scene');
 	
 ?>
